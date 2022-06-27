b21e7df8faf295ebaa28a0698a8ff9a681cd7fc3
Parktaehoon1 <92parkth@gmail.com>
2022-06-27 17:10:09 +0900

    1차 퍼블리싱 완료

--- js/script.js
+++ js/script.js
@@ -1,8 +1,37 @@
+// 멀티미디어 요소 로딩 전 실행
+$(document).ready();
 
-      var swiper = new Swiper(".sw-banner", {
-        pagination: {
-          el: ".swiper-pagination",
-          dynamicBullets: true,
-        },
-      });
-    
+// 멀티미디어 요소 로딩 완료 후 실행
+window.onload = function(){
+  // 상단 배너 슬라이드 
+  new Swiper('.sw-banner', {
+    loop:true,
+    // fade 효과 ( swiper demo 확인 )
+    effect: "fade",
+    // pagination( swiper demo 확인, click 가능 )
+    pagination: {
+      el: ".sw-banner-pg",
+      clickable: true
+    },
+    autoplay:{
+      delay: 2500,
+      disalbeOnInteraction: false,
+    },
+    // touchMove 제거 , 스와이퍼 api에서 찾아보기
+    allowTouchMove:false,
+  });
+
+  // visual 슬라이드
+  new Swiper('.sw-visual', {
+    loop:true,
+    // pagination( swiper demo 확인, click 가능 )
+    pagination: {
+      el: ".sw-visual-pg",
+      clickable: true
+    },
+    autoplay:{
+      delay: 2500,
+      disalbeOnInteraction: false,
+    },
+  });
+};
